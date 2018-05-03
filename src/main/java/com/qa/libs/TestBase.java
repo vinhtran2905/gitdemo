7d64fefce5a6044e5403395bb6eda9c0f53501d2
Vinh Tran <vinhtran2905@gmail.com>
2018-05-03 14:17:05 -0500

    first commit git demo

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,7 @@
+package com.qa.libs;
+
+public class TestBase {
+	public void test() {
+		System.out.println("test");
+	}
+}
---
+++ src/test/java/com/qa/tests/LoginPageTest.java
@@ -0,0 +1,7 @@
+package com.qa.tests;
+
+public class LoginPageTest {
+	public void Test_loginPage() {
+		System.out.println("test");
+	}
+}
